2c0bf4a963b61a393f83c41003be021243644590
ymca85 <bhushan.ymca85@gmail.com>
2016-12-03 00:48:14 +0530

    green - added logic for converting 21

--- src/main/NumberToWordConverter.java
+++ src/main/NumberToWordConverter.java
@@ -5,7 +5,16 @@ public class NumberToWordConverter {
 	public String convertNumberToWord(int number) {
 
 		DigitToWordMapper digitToWordMapper = new DigitToWordMapper();
-		
+
+		if (number > DigitInWord.TWENTY.numericValue) {
+
+			int digitAtTens = number / DigitInWord.TEN.numericValue;
+			int digitAtUnit = number % DigitInWord.TEN.numericValue;
+
+			return digitToWordMapper.getWordFromDigit(digitAtTens * DigitInWord.TEN.numericValue).name() + " "
+					+ digitToWordMapper.getWordFromDigit(digitAtUnit).name();
+		}
+
 		return digitToWordMapper.getWordFromDigit(number).name();
 	}
 }

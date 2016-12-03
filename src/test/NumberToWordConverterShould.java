b25c8207fdf2e70213b56b56de5218afa23d7cf4
ymca85 <bhushan.ymca85@gmail.com>
2016-12-03 20:10:12 +0530

    green - rearranged / updated names or test case and added few more cases

--- src/test/NumberToWordConverterShould.java
+++ src/test/NumberToWordConverterShould.java
@@ -9,27 +9,45 @@ import main.NumberToWordConverter;
 public class NumberToWordConverterShould {
 
 	@Test
-	public void return_ONE_for_1() {
-		NumberToWordConverter numberToWordConverter = new NumberToWordConverter();
-		assertEquals("ONE", numberToWordConverter.convertNumberToWord(1));
-	}
-	
-	@Test
 	public void return_ZERO_for_0() {
 		NumberToWordConverter numberToWordConverter = new NumberToWordConverter();
 		assertEquals("ZERO", numberToWordConverter.convertNumberToWord(0));
 	}
-	
+
 	@Test
-	public void return_NINE_for_2() {
+	public void return_ONE_for_1() {
+		NumberToWordConverter numberToWordConverter = new NumberToWordConverter();
+		assertEquals("ONE", numberToWordConverter.convertNumberToWord(1));
+	}
+
+	@Test
+	public void return_TWO_for_2() {
 		NumberToWordConverter numberToWordConverter = new NumberToWordConverter();
 		assertEquals("TWO", numberToWordConverter.convertNumberToWord(2));
 	}
-	
+
 	@Test
-	public void return_NINE_for_21() {
+	public void return_TWENTY_for_20() {
 		NumberToWordConverter numberToWordConverter = new NumberToWordConverter();
 		assertEquals("TWENTY ONE", numberToWordConverter.convertNumberToWord(21));
+	}
+
+	@Test
+	public void return_TWENTY_ONE_for_21() {
+		NumberToWordConverter numberToWordConverter = new NumberToWordConverter();
+		assertEquals("TWENTY ONE", numberToWordConverter.convertNumberToWord(21));
+	}
+
+	@Test
+	public void return_TWENTY_TWO_for_22() {
+		NumberToWordConverter numberToWordConverter = new NumberToWordConverter();
+		assertEquals("TWENTY TWO", numberToWordConverter.convertNumberToWord(22));
+	}
+
+	@Test
+	public void return_TWENTY_NINE_for_29() {
+		NumberToWordConverter numberToWordConverter = new NumberToWordConverter();
+		assertEquals("TWENTY NINE", numberToWordConverter.convertNumberToWord(29));
 	}
 
 }
